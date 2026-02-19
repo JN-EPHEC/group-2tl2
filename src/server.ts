70576e02959e96856d92b8ac5fcd071f377fb1a4
Timothée Cuny <timothee.cuny@gmail.com>
2026-02-19 15:46:59 +0100

    backup

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,39 @@
 import express from "express";
 import path from "path";
 
+// Imports Swagger
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger"; 
+
 import sequelize from "./config/database";
 import userRoutes from "./routes/userRoutes";
+import { requestLogger } from "./middlewares/logger";
+import { errorHandler } from "./middlewares/errorHandler";
 import "./models/User";
 
 const app = express();
 
 app.use(express.json());
 
-// Sert le dossier public (index.html et script.js)
+// Activation du logger global
+app.use(requestLogger); 
+
+// Activation de Swagger (En premier de la liste des routes)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Dossier public
 app.use(express.static(path.join(process.cwd(), "public")));
 
 // API users
 app.use("/api/users", userRoutes);
 
+// GESTION DES ERREURS : Doit être la dernière étape !
+app.use(errorHandler);
+
 sequelize
   .sync()
   .then(() => {
-    console.log("Connexion bdd sync ok");
+    console.log("Base de données synchronisée");
     app.listen(3000, () => {
       console.log("Serveur lancé sur http://localhost:3000");
     });
