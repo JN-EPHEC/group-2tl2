cd51687c4e57fab5dc023db413de3b78afd912cd
Timothée Cuny <timothee.cuny@gmail.com>
2026-02-19 21:22:55 +0100

    tp3 ex2.7.1

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,10 +3,44 @@ import * as userController from "../controllers/userController";
 
 const router = Router();
 
-// Ne remettez pas de commentaire Swagger ici, on l'a mis dans swagger.ts
-router.get('/', userController.getAllUsers);
+/**
+ * @swagger
+ * /api/users:
+ * get:
+ * summary: Récupère la liste des utilisateurs
+ * tags: [Users]
+ * responses:
+ * 200:
+ * description: Succès
+ */
+router.get("/", userController.getAllUsers);
 
+/**
+ * @swagger
+ * /api/users:
+ * post:
+ * summary: Crée un nouvel utilisateur
+ * tags: [Users]
+ * responses:
+ * 201:
+ * description: Utilisateur créé
+ */
 router.post("/", userController.createUser);
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ * delete:
+ * summary: Supprime un utilisateur
+ * tags: [Users]
+ * parameters:
+ * - in: path
+ * name: id
+ * required: true
+ * responses:
+ * 204:
+ * description: Supprimé avec succès
+ */
 router.delete("/:id", userController.deleteUser);
 
 export default router;
